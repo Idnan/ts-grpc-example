7257a29d75451c4f4c0426939327214fdc3517cb
Adnan Ahmed <idnan.se@gmail.com>
2019-11-02 12:50:40 +0400

    added comments

--- src/proto/index.ts
+++ src/proto/index.ts
@@ -1,5 +1,9 @@
+// register all your proto typescript definitions here
+// this way there respective javascript files in will include in the
+// compile phase
+
 import './greeter/greeter_pb';
 import './greeter/greeter_grpc_pb';
 
-export const protoIndex:any = ():void => {
+export const protoIndex: any = (): void => {
 };
